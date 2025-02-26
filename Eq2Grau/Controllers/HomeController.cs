f244003d17fd477060e87c718670d99526884712
brnas02 <aluno23025@ipt.pt>
2025-02-26 12:56:20 +0000

    Criação do Algoritmo

--- Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Controllers/HomeController.cs
@@ -4,28 +4,43 @@ using System.Diagnostics;
 
 namespace Eq2Grau.Controllers
 {
-    public class HomeController : Controller
-    {
+    public class HomeController : Controller{
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController(ILogger<HomeController> logger){
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
+        public IActionResult Index(){
+            /*Algoritmo
+             *1- ler parâmetros a, b, c
+             *2- verificar se os parâmetros são números
+             *      se não, criar mensagem de aviso
+             *      se sim, continuo
+             *3- a =/= 0?????
+             *      se não, mostro mensagem de aviso
+             *      se sim, continuo
+             *4- calcular Delta = b2-4ac
+             * 4.1- se Delta > 0, calcular raízes reais
+             *      x1 = (-b - sqrt(Delta))/2/a
+             *      x2 = (-b + sqrt(Delta))/2/a
+             * 4.2- se Delta = 0
+             *      x1 = x2 = (-b)/2/a
+             * 4.3- se Delta < 0, calcular raízes complexas, conjugadas
+             *      x1 = (-b)/2/a '+' sqrt(-Delta))/2/a 'i'
+             *      x2 = (-b)/2/a '-' sqrt(-Delta))/2/a 'i'
+             *5- mostrar o resultado na View
+            */
+
             return View();
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult Privacy(){
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
+        public IActionResult Error(){
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
